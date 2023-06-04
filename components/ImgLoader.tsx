421fdf4344957b2f6505ceb121ca79633186b6b1
rajanlagah <rajanlagah@gmail.com>
2023-06-05 01:51:07 +0530

    Paypal docs added

--- components/ImgLoader.tsx
+++ components/ImgLoader.tsx
@@ -13,13 +13,18 @@ const ImgLoader = ({type}) =>  {
 
   const TYPE_TO_IMG_MAP = {
     nft_landing_page: {
-      url:"https://nft-ranking-weld.vercel.app/"
+      url:"https://nft-ranking-weld.vercel.app/",
+      src:"/NFT_ranking_landing_page.png"
+    },
+    paypal_landing_page: {
+      url:"https://paypal-decentralized.vercel.app",
+      src:"/Paypal/PayPal_LandingPage_1.png"
     }
   }
 
   return (
     <figure style={{textAlign:"center"}}>
-      <img src={'/NFT_ranking_landing_page.png'} />
+      <img src={TYPE_TO_IMG_MAP[type].src} />
       <a className={styles["img-caption-demo"]} href={TYPE_TO_IMG_MAP[type].url}>Visit website ↗</a>
     </figure>
   )
